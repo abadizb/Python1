9cba88cc2fd6ac5b2f44d8b7ab7479acbfb90100
abadizb <abadizb@hotmail.com>
2014-10-28 20:12:11 -0400

    Update homework 3
    Update homework 3

--- Homework3/data_load.py
+++ Homework3/data_load.py
@@ -5,27 +5,26 @@ import sys
 from quotes import datalist
 
 
-def get_traversal_data():
+
+
+def traverser():
     list1 = []
-    start_dir = 'C:\Users\Halim\Python34'
+    start_dir = 'C:\\Users\\Halim\\Python34\\fortune1'
 
     for directory, folders, files in os.walk(start_dir):
             for single_file in files:
-                    if (fnmatch.fnmatch(single_file, "*txt") or fnmatch.fnmatch(single_file, "*log")):
-                        #print("Reading... ", single_file)
-                        filepath = (os.path.join(directory, single_file))
-                        f = open(filepath)
-                        #print("File Content...")
-                        fileData = f.read()
-                        filetuple = (filepath,fileData)
-                        list1.append (filetuple)
-                        
-    f.close()
-    file1 = os.path.join (os.getcwd(), 'single_file' )                   
-    pickledata = open (file1,"bw")
+                    if (fnmatch.fnmatch(single_file, "*txt")):
+                            #print("Reading... ", single_file)
+                            filepath = (os.path.join(directory, single_file))
+                            f = open(filepath)
+                            #print("File Content...")
+                            fileData = f.read()
+                            filetuple = (filepath,fileData)
+                            list1.append (filetuple)
+                            f.close()
+    pickledata = open ("raw_data.txt","bw")
     pickle.dump(list1,pickledata)
     pickledata.close()
     
 
-get_traversal_data()
-
+traverser()
--- Homework3/search_combine.py
+++ Homework3/search_combine.py
@@ -2,12 +2,12 @@ import searcher
 import data_load
 import indexer
 import quotes
-import traverser
 
-traverser.traverser()
+
+
 indexer.indexer()
 searcher.searcher()
-data_load.get_traversal_data()
+data_load.traverser()
 
 d = indexer.indexer("raw_data.pickle")
 searcher.search(d)
